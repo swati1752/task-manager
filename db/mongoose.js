46cb71edcbea0727fa0dfb8dc555dbbd66ef25c1
swati1752 <73819373+swati1752@users.noreply.github.com>
2022-04-16 14:55:57 +0530

    Update mongoose.js

--- db/mongoose.js
+++ db/mongoose.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const validator = require('validator');
 if (process.env.NODE_ENV !== 'production') 
 require ('dotenv').config()
-// const db_link = 'mongodb+srv://admin:Password%40123@cluster0.blhys.mongodb.net/task-manager?retryWrites=true&w=majority' 
+ 
 
 
 mongoose.connect( process.env.MONGO_URL ,
